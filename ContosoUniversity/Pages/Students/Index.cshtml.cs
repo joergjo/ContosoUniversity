17b662488c045b2796aa0ac8af3fdea6d4e2ea60
Jörg Jooss <joerg@joergjooss.de>
2021-12-19 12:01:38 +0100

    Implement part 3

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -8,17 +8,62 @@ namespace ContosoUniversity.Pages.Students
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(SchoolContext context)
+        public IndexModel(SchoolContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
-        public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            Student = await _context.Students.ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString is not null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+            var studentsQuery = from s in _context.Students select s;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                studentsQuery = studentsQuery.Where(s =>
+                    s.LastName.Contains(searchString) || s.FirstMidName.Contains(searchString));
+            }
+            
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsQuery = studentsQuery.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsQuery = studentsQuery.OrderBy(s => s.EnrollmentDate);
+                    break;  
+                case "date_desc":
+                    studentsQuery = studentsQuery.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsQuery = studentsQuery.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            var pageSize = _configuration.GetValue("PageSize", 4);
+            Students = await PaginatedList<Student>.CreateAsync(studentsQuery.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
